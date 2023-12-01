b237ea71f5b3de6151dc93b37991d92d93c1b81c
SK <12840358+null1103@users.noreply.github.com>
2023-11-30 19:11:38 -0800

    49. Group Anagrams

---
+++ GroupAnagrams.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+/**
+ * https://leetcode.com/problems/group-anagrams
+ * 
+ * HASHMAP
+ * - Iterate through "strs" and for each string "str":
+ *   - Store count of each character in the count array. Convert to string to use as map's key.
+ *   - Add str to the corresponding value list for the key
+ * 
+ * Time:  O(m * n logn)  
+ * Space: O(m * n)
+ */
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String str: strs) {
+            int[] count = new int[26];
+            for (Character ch: str.toCharArray()) {
+                count[ch-'a'] += 1;
+            }
+            String key = Arrays.toString(count);
+            if(!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
