6d6e492b17a17d32660a3dfefdde9efa02575e80
SK <12840358+null1103@users.noreply.github.com>
2023-12-01 22:33:06 -0800

    125. Valid Palindrome

---
+++ ValidPalindrome.java
@@ -0,0 +1,36 @@
+/**
+ * https://leetcode.com/problems/valid-palindrome
+ * 
+ * Use two pointers for the start and end of string
+ * While start < end:
+ * - If the current chars are not alphanumeric, increment start or decrement end accordingly
+ * - If alphanumeric chars at start and end are not equal (ignore case), return false
+ * - If chars at start and end are equal (ignore case), increment start and decrement end
+ * 
+ * Time:  O(n)
+ * Space: O(1)
+ */
+class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length()-1;
+        while (start < end) {
+            Character startChar = s.charAt(start);
+            Character endChar = s.charAt(end);
+            if (!Character.isLetterOrDigit(startChar)) {
+                start++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(endChar)) {
+                end--;
+                continue;
+            }
+            if (Character.toLowerCase(startChar) != Character.toLowerCase(endChar)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;   
+    }
+}
